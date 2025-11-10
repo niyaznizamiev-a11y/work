4820c701450d36e00d6c5c7935e1f9e74a4e4fca
niyaznizamiev-a11y <niyaznizamiev@gmail.com>
2025-11-10 12:26:18 +0300

    error

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
@@ -13,6 +14,29 @@ const config: StorybookConfig = {
   "framework": {
     "name": "@storybook/react-webpack5",
     "options": {}
-  }
+  },
+  webpackFinal: async (config) => {
+    // Найдите правило для обработки CSS
+    const cssRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test(".css")
+    );
+
+    if (cssRule) {
+      // Замените существующее правило на новое
+      cssRule.use = [
+        "style-loader",
+        "css-loader",
+        {
+          loader: "postcss-loader",
+          options: {
+            postcssOptions: {
+              plugins: ["tailwindcss", "autoprefixer"],
+            },
+          },
+        },
+      ];
+    }
+    return config;
+  },
 };
 export default config;
--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '../../../../node_modules/@storybook/react/dist/index';
+import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from './Button';
 
 const meta = {
